cb899f08a0a17a5a91b380d041149fb2f8fbd77e
Hendry Sadrak <hendry.sadrak@outlook.com>
2016-08-30 12:49:40 +0300

    [major] Convert from es6 to es5

--- crouch.js
+++ crouch.js
@@ -7,7 +7,7 @@
  * http://regexr.com/3e1o7
  * @type {RegExp}
  */
-const _re = /{([0-9a-zA-Z]+?)}/g;
+var _re = /{([0-9a-zA-Z]+?)}/g;
 
 
 /**
@@ -17,18 +17,32 @@ const _re = /{([0-9a-zA-Z]+?)}/g;
  * @param {array|object} values
  * @returns {string}
  */
-const crouch = ( template, values ) => {
-    let match;
+var crouch = function ( template, values ) {
+    /*
+     * Default arguments
+     */
+    var
+        template = template || "",
+        values   = values || {};
 
-    // Loop through all the placeholders that matched with regex
+    var match;
+
+    /*
+     * Loop through all the placeholders that matched with regex
+     */
     while ( match = _re.exec( template ) ) {
-        let _value = values[ match[ 1 ] ];
+        var _value = values[ match[ 1 ] ];
         if ( !_value ) _value = "";
 
-        // Replace the placeholder with a real value.
+        /*
+         * Replace the placeholder with a real value.
+         */
         template = template.replace( match[ 0 ], _value )
     }
 
+    /*
+     * Return the template with filled in values
+     */
     return template;
 };
 
