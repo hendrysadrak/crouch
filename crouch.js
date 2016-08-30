f628c4a6f1b9c335814b4944a5d498331f101941
Hendry Sadrak <hendry.sadrak@outlook.com>
2016-08-30 12:43:42 +0300

    [major] Initial code commit

--- crouch.js
+++ crouch.js
@@ -1,2 +1,39 @@
 "use strict";
 
+
+/**
+ * RegExp to find placeholders in the template
+ *
+ * http://regexr.com/3e1o7
+ * @type {RegExp}
+ */
+const _re = /{([0-9a-zA-Z]+?)}/g;
+
+
+/**
+ * Micro template compiler
+ *
+ * @param {string} template
+ * @param {array|object} values
+ * @returns {string}
+ */
+const crouch = ( template, values ) => {
+    let match;
+
+    // Loop through all the placeholders that matched with regex
+    while ( match = _re.exec( template ) ) {
+        let _value = values[ match[ 1 ] ];
+        if ( !_value ) _value = "";
+
+        // Replace the placeholder with a real value.
+        template = template.replace( match[ 0 ], _value )
+    }
+
+    return template;
+};
+
+
+/*
+ * Export
+ */
+module.exports = crouch;
