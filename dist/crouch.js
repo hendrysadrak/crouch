324a7c821f3878c24de56caf3a3fa7f034baebfe
Hendry Sadrak <hendry.sadrak@outlook.com>
2018-01-12 17:50:47 +0200

    v0.1.2

--- dist/crouch.js
+++ dist/crouch.js
@@ -1,9 +1,9 @@
 /**!
- * Crouch.js – v0.1.0
- * Compiled Thu Dec 29 2016 14:09:51 GMT+0200 (EET)
+ * Crouch.js – v0.1.2
+ * Compiled Fri Jan 12 2018 17:50:25 GMT+0200 (EET)
  * @author: Hendry Sadrak (https://www.hendrysadrak.com)
  * @license: MIT
  * http://www.opensource.org/licenses/mit-license
  * see: https://github.com/hendrysadrak/crouch for details
  */
-'use strict';(function(d,a,b){"function"===typeof define&&define.amd?define([],b):"object"===typeof module&&module.exports?module.exports=b():d[a]=b()})(this,"crouch",function(){var d=/{([0-9a-z_]+?)}/ig;return function(a,b){a=a||"";b=b||{};for(var e;e=d.exec(a);){var c=b[e[1]];if(null===c||"undefined"===typeof c)c="";c=String(c);a=a.replace(e[0],c)}return a}});
+'use strict';(function(a,c,b){"function"===typeof define&&define.amd?define([],b):"object"===typeof module&&module.exports?module.exports=b():a[c]=b()})(this,"crouch",function(){return function(a,c){a=a||"";c=c||{};for(var b;b=/{([0-9a-z_]+?)}/ig.exec(a);){var d=c[b[1]];null==d&&(d="");d=String(d);a=a.replace(b[0],d)}return a}});
