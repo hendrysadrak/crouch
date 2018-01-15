a0478f9f17609394a9fbf95dc40bc7c439cf1ddd
Hendry Sadrak <hendry.sadrak@outlook.com>
2018-01-15 11:32:58 +0200

    Convert source to es6

--- dist/crouch.js
+++ dist/crouch.js
@@ -1,9 +1,9 @@
 /**!
  * Crouch.js – v0.1.2
- * Compiled Mon Jan 15 2018 11:26:18 GMT+0200 (EET)
+ * Compiled Mon Jan 15 2018 11:31:27 GMT+0200 (EET)
  * @author: Hendry Sadrak (https://www.hendrysadrak.com)
  * @license: MIT
  * http://www.opensource.org/licenses/mit-license
  * see: https://github.com/hendrysadrak/crouch for details
  */
-'use strict';(function(a,c,b){"function"===typeof define&&define.amd?define([],b):"object"===typeof module&&module.exports?module.exports=b():a[c]=b()})(this,"crouch",function(){return function(a,c){a=a||"";c=c||{};for(var b;b=/{([0-9a-z_]+?)}/ig.exec(a);){var d=c[b[1]];null==d&&(d="");d=String(d);a=a.replace(b[0],d)}return a}});
+'use strict';(function(b,c,a){"function"===typeof define&&define.amd?define([],a):"object"===typeof module&&module.exports?module.exports=a():b[c]=a()})(this,"crouch",function(){return function(b,c){for(var a;a=/{([0-9a-z_]+?)}/ig.exec(b);)b=b.replace(a[0],String(c[a[1]]||""));return b}});
